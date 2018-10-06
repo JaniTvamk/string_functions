cde18d04da011b0396bf368598f62092bc683424
JaniTvamk <e1401472@edu.vamk.fi>
2018-10-06 12:33:07 +0300

    Added comments to functions

--- string.c
+++ string.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int my_strlen(char *s)
+int my_strlen(char *s) // Use this function to calculate the lenght of a string.
 {
 	int i=0;
 	while(*(s+i) != '\0')
@@ -14,7 +14,7 @@ int my_strlen(char *s)
 
 }
 
-int str2upper(char *s)
+int str2upper(char *s)  //Use this function to convert lower case english alphabet to upper case
 {
 int i = 0;
 int j=0;
@@ -34,7 +34,7 @@ int j=0;
 return j;
 }
 
-int str2lower_(char *s)
+int str2lower_(char *s) // Use this function to convert upper case english alphabet to lower case
 {
 	int i = 0;
 	int j=0;
@@ -55,7 +55,7 @@ int str2lower_(char *s)
 	return j;
 }
 
-int str_strip_numbers(unsigned char *s)
+int str_strip_numbers(unsigned char *s)// Use this function to strip a string from numbers and returns the new lenght of the string(Doesn't take work with two or more numbers back to back).
 {
 	int i = 0;
 	int j=0;
@@ -91,7 +91,7 @@ int str_strip_numbers(unsigned char *s)
 	return len_new;
 }
 
-void mystrcpy(char *s,char *d)
+void mystrcpy(char *s,char *d)//use this function to copy a string to another variable
 {
 	int j=0;
 	while (*(d+j) != '\0')
@@ -104,7 +104,7 @@ void mystrcpy(char *s,char *d)
 }
 
 
-int mystrcmp (char *s, char *d)
+int mystrcmp (char *s, char *d)//use this function to compare two strings.(Returns the position of firt different character and tells which one is before in the alphabet)
 {
 	int i=0;
 	while(*(s+i) != '\0' && *(d+i) != '\0' && *(s+i) == *(d+i)){
@@ -125,7 +125,7 @@ int mystrcmp (char *s, char *d)
 }
 
 
-char * strdupl(char *s)
+char * strdupl(char *s)//use this function to make a duplicate of your string.(Returns a pointer to new variable with your string).
 {
 	int length = my_strlen(s);
 	char *a;
